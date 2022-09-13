fa9d2b5d159105fc6d39a841b0bd8cd218b24c46
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-09-13 15:52:17 -0700

    split date

--- src/Components/ArticleCard/ArticleCard.js
+++ src/Components/ArticleCard/ArticleCard.js
@@ -2,11 +2,17 @@ import React from 'react'
 import "./ArticleCard.css"
 
 const ArticleCard = ({title, description, author, link, image, published, id}) => {
+
+  const splitDate = () => {
+    return published.split('T')[0]
+
+  }
+
   return (
     <div className="article-card">
-      <h3>{title}</h3>
+      <h3>Title: {title}</h3>
       <p>{author}</p>
-      <p>{published}</p>
+      <p>Published Date: {splitDate()}</p>
     </div>
   )
 }
