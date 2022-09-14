829ca8ee30ff74a2a072dd6dd489ed75fe630d18
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-09-13 18:19:39 -0700

    update show page with more attributes, add link to NYT site

--- src/Components/Showpage/Showpage.js
+++ src/Components/Showpage/Showpage.js
@@ -1,12 +1,33 @@
 import React from 'react'
 import { useLocation } from "react-router";
+import "./Showpage.css"
 
 
 const Showpage = () => {
   const { state: { article } } = useLocation()
 
   return (
-    <div>{article.title}</div>
+    <div className="showpage-container">
+      <div className="info-container">
+        <div className="showpage-title">
+          {article.title}
+        </div>
+        <div className="showpage-description">
+          {article.description}
+        </div>
+        <div className="showpage-author">
+          {article.author}
+        </div>
+        <div className="showpage-publication">
+          {article.published}
+        </div>
+
+        <form action={article.link}>
+          <input type="submit" value="Go to NYT" />
+        </form>
+
+      </div>
+    </div>
   )
 }
 
