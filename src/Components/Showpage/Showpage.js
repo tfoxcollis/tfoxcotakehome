e100420b30d2c1c09ee149df24e5d727e3f41b14
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-09-13 18:57:40 -0700

    get image to show on showpage, minor css

--- src/Components/Showpage/Showpage.js
+++ src/Components/Showpage/Showpage.js
@@ -10,18 +10,22 @@ const Showpage = () => {
     <div className="showpage-container">
       <div className="info-container">
         <div className="showpage-title">
-          {article.title}
-        </div>
-        <div className="showpage-description">
-          {article.description}
-        </div>
-        <div className="showpage-author">
-          {article.author}
-        </div>
-        <div className="showpage-publication">
-          {article.published}
+          <h1>{article.title}</h1>
         </div>
 
+        <img src={article.image} className="image"/>
+        
+        <div className="showpage-author">
+         <p>{article.author}</p>
+        </div>
+
+        <div className="showpage-description">
+          <h3>{article.description}</h3>
+        </div>
+        <div className="showpage-publication">
+          <p>{article.published}</p>
+        </div>
+    
         <form action={article.link}>
           <input type="submit" value="Go to NYT" />
         </form>
